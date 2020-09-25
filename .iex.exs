b97846a2cddb451a9c38835aab9f295a1ea4afa5
Joseph Lozano <joseph@hey.com>
2020-09-24 23:12:48 -0400

    extract out some functionality

--- .iex.exs
+++ .iex.exs
@@ -1,19 +1,55 @@
 alias Votex.Schulze
-candidates = %{a: "Alice", b: "Bob", c: "Charlie", d: "Doug", e: "Ellie"}
+candidates = %{A: "Alice", B: "Bob", C: "Charlie", D: "Doug", E: "Ellie"}
 candidate_names = Map.values(candidates)
 
-# https://en.wikipedia.org/wiki/Schulze_method#Example
-votes =
-  %{
-    ~w(a c b e d)a => 5,
-    ~w(a d e c b)a => 5,
-    ~w(b e d a c)a => 8,
-    ~w(c a b e d)a => 3,
-    ~w(c a e b d)a => 7,
-    ~w(c b a d e)a => 2,
-    ~w(d c e b a)a => 7,
-    ~w(e b a d c)a => 8
-  }
+# https://electowiki.org/wiki/Schulze_method#Example_1
+votes_1 = %{
+  ~w(A C B E D)a => 5,
+  ~w(A D E C B)a => 5,
+  ~w(B E D A C)a => 8,
+  ~w(C A B E D)a => 3,
+  ~w(C A E B D)a => 7,
+  ~w(C B A D E)a => 2,
+  ~w(D C E B A)a => 7,
+  ~w(E B A D C)a => 8
+}
+
+# https://electowiki.org/wiki/Schulze_method#Example_2
+votes_2 = %{
+  ~w(A C B D)a => 5,
+  ~w(A C D B)a => 2,
+  ~w(A D C B)a => 3,
+  ~w(B A C D)a => 4,
+  ~w(C B D A)a => 3,
+  ~w(C D B A)a => 3,
+  ~w(D A C B)a => 1,
+  ~w(D B A C)a => 5,
+  ~w(D C B A)a => 4
+}
+
+# https://electowiki.org/wiki/Schulze_method#Example_3
+votes_3 = %{
+  ~w(A B D E C)a => 3,
+  ~w(A D E B C)a => 5,
+  ~w(A D E C B)a => 1,
+  ~w(B A D E C)a => 2,
+  ~w(B D E C A)a => 2,
+  ~w(C A B D E)a => 4,
+  ~w(C B A D E)a => 6,
+  ~w(D B E C A)a => 2,
+  ~w(D E C A B)a => 5
+}
+
+# https://electowiki.org/wiki/Schulze_method#Example_4
+votes_4 = %{
+  ~w(A B C D)a => 3,
+  ~w(D A B C)a => 2,
+  ~w(D B C A)a => 2,
+  ~w(C B D A)a => 2
+}
+
+get_votes = fn votes ->
+  votes
   |> Enum.flat_map(fn {ordering, times} ->
     Enum.map(1..times, fn _ ->
       ordering
@@ -24,14 +60,31 @@ votes =
       end)
     end)
   end)
+end
 
-{:ok, fresh_ballot} = Schulze.new_ballot(candidate_names)
+get_ballot = fn votes ->
+  candidate_names = votes |> Map.keys() |> hd |> Enum.map(&Map.get(candidates, &1))
+  {:ok, ballot} = Schulze.new_ballot(candidate_names)
 
-ballot =
-  Enum.reduce(votes, fresh_ballot, fn vote, acc ->
+  Enum.reduce(get_votes.(votes), ballot, fn vote, acc ->
     with {:ok, b} <- Schulze.cast_vote(acc, vote) do
       b
     end
   end)
+end
 
-# Schulze.get_winner(ballot)
+winner_1 =
+  Schulze.get_winner(get_ballot.(votes_1))
+  |> IO.inspect(label: 1)
+
+winner_2 =
+  Schulze.get_winner(get_ballot.(votes_2))
+  |> IO.inspect(label: 2)
+
+winner_3 =
+  Schulze.get_winner(get_ballot.(votes_3))
+  |> IO.inspect(label: 3)
+
+winner_4 =
+  Schulze.get_winner(get_ballot.(votes_4))
+  |> IO.inspect(label: 4)
--- lib/votex/schulze.ex
+++ lib/votex/schulze.ex
@@ -21,7 +21,7 @@ defmodule Votex.Schulze do
     end
   end
 
-  def get_winner(ballot) do
+  def get_results(ballot) do
     candidates = ballot.candidates
     votes = ballot.votes
 
@@ -31,79 +31,101 @@ defmodule Votex.Schulze do
       end
       |> Enum.reject(&is_nil(&1))
 
-    pairwise =
-      pairs
-      |> Enum.map(fn {c1, c2} ->
-        c1_preferred =
-          Enum.reduce(votes, 0, fn vote, acc ->
-            c1_vote = Map.get(vote, c1, 0)
-            c2_vote = Map.get(vote, c2, 0)
-            if c1_vote > c2_vote, do: acc + 1, else: acc
-          end)
-
-        {{c1, c2}, c1_preferred}
-      end)
-      |> Enum.group_by(fn {{c1, c2}, _} -> Enum.sort([c1, c2]) end, fn {{c1, _}, preference} ->
-        {c1, preference}
-      end)
-      |> Enum.map(fn {_pairing, [{c1, c1_votes}, {c2, c2_votes}]} ->
-        cond do
-          c1_votes > c2_votes -> {{c1, c2}, c1_votes}
-          c2_votes > c1_votes -> {{c2, c1}, c2_votes}
-          c1_votes == c2_votes -> nil
-        end
-      end)
-      |> Enum.reject(&is_nil(&1))
-
-    g =
-      Graph.new()
-      |> Graph.add_vertices([candidates])
-
-    g =
-      Enum.reduce(pairwise, g, fn {{c1, c2}, weight}, graph ->
-        Graph.add_edge(graph, c2, c1, weight: weight)
-      end)
+    pairwise_winners = get_pairwise_winners(votes, pairs)
+    g = build_graph(candidates, pairwise_winners)
 
     strongest_paths =
       pairs
       |> Enum.map(fn {c1, c2} = pair ->
-        weight =
-          Graph.get_paths(g, c1, c2)
-          |> Enum.map(fn path ->
-            path
-            |> Enum.with_index()
-            |> Enum.reduce([], fn {node, i}, acc ->
-              next = Enum.at(path, i + 1)
-
-              case next do
-                nil -> acc
-                _ -> acc ++ Graph.edges(g, node, next)
-              end
-            end)
-            |> Enum.min_by(& &1.weight)
-          end)
-          |> Enum.max_by(& &1.weight)
-          |> Map.get(:weight)
-
+        weight = get_strongest_path_weight(g, c1, c2)
         {pair, weight}
       end)
 
-    results =
-      strongest_paths
-      |> Enum.group_by(fn {{c1, c2}, _} -> Enum.sort([c1, c2]) end, fn {{c1, _}, strength} ->
-        {c1, strength}
-      end)
-      |> Enum.map(fn {_pairing, [{c1, c1_votes}, {c2, c2_votes}]} ->
-        cond do
-          c1_votes > c2_votes -> c1
-          c2_votes > c1_votes -> c2
-          c1_votes == c2_votes -> nil
-        end
-      end)
-      |> Enum.reject(&is_nil(&1))
-      |> Enum.frequencies()
+    strongest_paths
+    |> Enum.group_by(fn {{c1, c2}, _} -> Enum.sort([c1, c2]) end, fn {{c1, _}, strength} ->
+      {c1, strength}
+    end)
+    |> Enum.map(fn {_pairing, [{c1, c1_votes}, {c2, c2_votes}]} ->
+      cond do
+        c1_votes > c2_votes -> c1
+        c2_votes > c1_votes -> c2
+        c1_votes == c2_votes -> nil
+      end
+    end)
+    |> Enum.reject(&is_nil(&1))
+    |> Enum.frequencies()
+  end
 
-    results
+  defp get_pairwise_winners(votes, pairs) do
+    pairs
+    |> Enum.map(fn {c1, c2} ->
+      c1_preferred =
+        Enum.reduce(votes, 0, fn vote, acc ->
+          c1_vote = Map.get(vote, c1, 0)
+          c2_vote = Map.get(vote, c2, 0)
+          if c1_vote > c2_vote, do: acc + 1, else: acc
+        end)
+
+      {{c1, c2}, c1_preferred}
+    end)
+    |> Enum.group_by(fn {{c1, c2}, _} -> Enum.sort([c1, c2]) end, fn {{c1, _}, preference} ->
+      {c1, preference}
+    end)
+    |> Enum.map(fn {_pairing, [{c1, c1_votes}, {c2, c2_votes}]} ->
+      cond do
+        c1_votes > c2_votes -> {{c1, c2}, c1_votes}
+        c2_votes > c1_votes -> {{c2, c1}, c2_votes}
+        c1_votes == c2_votes -> nil
+      end
+    end)
+    |> Enum.reject(&is_nil(&1))
+  end
+
+  defp build_graph(candidates, pairwise_winners) do
+    g =
+      Graph.new()
+      |> Graph.add_vertices([candidates])
+
+    Enum.reduce(pairwise_winners, g, fn {{c1, c2}, weight}, graph ->
+      Graph.add_edge(graph, c2, c1, weight: weight)
+    end)
+  end
+
+  defp get_strongest_path_weight(graph, node1, node2) do
+    case Graph.get_paths(graph, node1, node2) do
+      [] ->
+        0
+
+      paths ->
+        paths
+        |> Enum.map(fn path ->
+          path
+          |> Enum.with_index()
+          |> Enum.reduce([], fn {node, i}, acc ->
+            next = Enum.at(path, i + 1)
+
+            case next do
+              nil -> acc
+              _ -> acc ++ Graph.edges(graph, node, next)
+            end
+          end)
+          |> Enum.min_by(& &1.weight)
+        end)
+        |> Enum.max_by(& &1.weight)
+        |> Map.get(:weight)
+    end
+  end
+
+  def get_winner(%Ballot{} = ballot), do: get_results(ballot) |> get_winner(ballot)
+
+  def get_winner(results, ballot) when is_map(results) do
+    (ballot.candidates -- Map.keys(results))
+    |> Enum.reduce(results, fn missing, acc -> Map.put(acc, missing, 0) end)
+    |> Enum.group_by(fn {_candidate, strength} -> strength end, fn {candidate, _strength} ->
+      candidate
+    end)
+    |> Enum.sort_by(fn {score, _} -> score end, &(&1 >= &2))
+    |> Enum.map(&elem(&1, 1))
   end
 
   defp validate(ballot, vote) do
