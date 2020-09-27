12b0eb2bb57f8c37bafba52bcf72caabbc64d818
Joseph Lozano <joseph@hey.com>
2020-09-26 21:57:01 -0400

    setup for release

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,19 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :schulze, Schulze.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -43,7 +55,7 @@ config :schulze, Schulze.Mailer,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :schulze, SchulzeWeb.Endpoint, server: true
+config :schulze, SchulzeWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
