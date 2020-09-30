ad8d9f35c56566995e4775561d2ed78184abb89b
Joseph Lozano <joseph@hey.com>
2020-09-30 00:01:20 -0400

    more refactoring

--- lib/schulze/impl.ex
+++ lib/schulze/impl.ex
@@ -167,15 +167,14 @@ defmodule Schulze.Impl do
     votes = election.votes
     pairs = get_pairs(candidates)
     pairwise = Enum.map(pairs, fn {c1, c2} -> count_times_preferred(c1, c2, votes) end)
-    pairwise_winners = get_pairwise_winners(pairwise)
-    g = build_graph(candidates, pairwise_winners)
+    g = build_graph(candidates, pairwise)
 
     results =
       Enum.map(pairs, fn {c1, c2} = pair ->
         {pair, get_strongest_path_weight(g, c1, c2)}
       end)
       |> get_pairwise_winners()
-      |> Enum.frequencies_by(fn {{c1, _}, _} -> c1 end)
+      |> Enum.frequencies()
       |> transpose_map()
       |> Enum.sort_by(fn {key, _} -> key end, &(&1 >= &2))
       |> Enum.map(fn {_, value} -> value end)
@@ -211,8 +210,8 @@ defmodule Schulze.Impl do
     |> Enum.group_by(&sorted_pair/1, fn {{c1, _}, preference} -> {c1, preference} end)
     |> Enum.map(fn {_pairing, [{c1, c1_votes}, {c2, c2_votes}]} ->
       cond do
-        c1_votes > c2_votes -> {{c1, c2}, c1_votes}
-        c2_votes > c1_votes -> {{c2, c1}, c2_votes}
+        c1_votes > c2_votes -> c1
+        c2_votes > c1_votes -> c2
         c1_votes == c2_votes -> nil
       end
     end)
@@ -230,31 +229,26 @@ defmodule Schulze.Impl do
   end
 
   defp get_path_strength(graph, path) do
-    path
-    |> Enum.with_index()
-    |> Enum.reduce([], fn {node, i}, acc ->
-      next = Enum.at(path, i + 1)
+    get_path_strength(graph, path, [])
+  end
 
-      case next do
-        nil -> acc
-        _ -> acc ++ Graph.edges(graph, node, next)
-      end
-    end)
-    |> Enum.min_by(& &1.weight)
+  defp get_path_strength(_graph, [_node | []], acc) do
+    acc |> Enum.min()
+  end
+
+  defp get_path_strength(graph, [node | tail], acc) do
+    [edge] = Graph.edges(graph, node, hd(tail))
+    get_path_strength(graph, tail, [edge.weight | acc])
   end
 
   defp get_strongest_path_weight(graph, node1, node2) do
-    case Graph.get_paths(graph, node1, node2) do
-      [] ->
-        0
-
-      paths ->
-        paths
-        |> Enum.map(&get_path_strength(graph, &1))
-        |> Enum.max_by(& &1.weight)
-        |> Map.get(:weight)
-    end
+    Graph.get_paths(graph, node1, node2)
+    |> Enum.map(&get_path_strength(graph, &1))
+    |> max_or_else(0)
   end
+
+  defp max_or_else([], x), do: x
+  defp max_or_else(list, _), do: Enum.max(list)
 
   defp validate(election, vote) do
     vote
